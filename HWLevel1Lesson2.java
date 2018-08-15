1bc230ec16a18324ccedf962c2a6c445d70f3c5a
DenisK <kuzovin@gmail.com>
2018-08-15 12:02:50 +0300

    Reformat code

--- HWLevel1Lesson2.java
+++ HWLevel1Lesson2.java
@@ -1,9 +1,9 @@
 /**
-*   Java Level 1. Lesson 2. Homework.
-*
-*   @author Denis Kuzovin
-*   @version Aug 15, 2018
-*/
+ * Java Level 1. Lesson 2. Homework.
+ *
+ * @author Denis Kuzovin
+ * @version Aug 15, 2018
+ */
 
 import java.util.Arrays;
 
@@ -44,13 +44,13 @@ public class HWLevel1Lesson2 {
         System.out.println("*** Task 4 ***");
         int[][] arr4 = new int[10][10];
         System.out.println("Original:");
-        for(int i = 0; i < arr4.length; i++) {
+        for (int i = 0; i < arr4.length; i++) {
             System.out.println(Arrays.toString(arr4[i]));
         }
         fillDiagonals(arr4);
         System.out.println();
         System.out.println("Modified:");
-        for(int i = 0; i < arr4.length; i++) {
+        for (int i = 0; i < arr4.length; i++) {
             System.out.println(Arrays.toString(arr4[i]));
         }
         System.out.println();
@@ -104,7 +104,7 @@ public class HWLevel1Lesson2 {
         System.out.println("Original:\t" + Arrays.toString(arr7_4));
         makeShiftRing(arr7_4, -20);
         System.out.println("Modified:\t" + Arrays.toString(arr7_4));
-
+        
         System.out.println("Shift right by ring version 2");
         System.out.println("Original:\t" + Arrays.toString(arr7_5));
         makeShiftRingNoArray(arr7_5, 3);
@@ -114,140 +114,141 @@ public class HWLevel1Lesson2 {
         makeShiftRingNoArray(arr7_6, -21);
         System.out.println("Modified:\t" + Arrays.toString(arr7_6));
     }
-
+    
     // Task 1
     static void invertData(int[] arr) {
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0)
                 arr[i] = 1;
             else
                 arr[i] = 0;
         }
     }
-
+    
     //Task 2
     static void fillData(int[] arr) {
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = i * 3;
         }
     }
-
+    
     //Task 3
     static void multiplySmallNumbers(int[] arr) {
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] < 6)
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6)
                 arr[i] *= 2;
         }
     }
-
+    
     //Task 4
     static void fillDiagonals(int[][] arr) {
-        for(int i = 0, j = 0; i < arr.length && j < arr[i].length; i++, j++) {
+        for (int i = 0, j = 0; i < arr.length && j < arr[i].length; i++, j++) {
             arr[i][j] = 1;
             arr[arr.length - i - 1][j] = 1;
         }
     }
-
+    
     //Task 5
     static void findMinAndMax(int[] arr, MinMax result) {
         result.min = arr[0];
         result.max = arr[0];
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] > result.max) result.max = arr[i];
-            if(arr[i] < result.min) result.min = arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > result.max) result.max = arr[i];
+            if (arr[i] < result.min) result.min = arr[i];
         }
     }
-
+    
     //Task 6
     static boolean checkBalance(int[] arr) {
         int sum1 = 0, sum2 = 0;
-
-        for(int i : arr) {
-             sum2 += i;
+        
+        for (int i : arr) {
+            sum2 += i;
         }
-
-        for(int i = 0; i < arr.length; i++) {
+        
+        for (int i = 0; i < arr.length; i++) {
             sum1 += arr[i];
             sum2 -= arr[i];
-            if(sum1 == sum2) return true;
+            if (sum1 == sum2) return true;
         }
         return false;
     }
-
+    
     //Task 7 (Shift with data loss)
     static void makeShiftN(int[] arr, int n) {
-        if(n > 0) {
-            for(int i = arr.length - 1 - n; i >= 0; i--) {
-                arr[i+n] = arr[i];
+        if (n > 0) {
+            for (int i = arr.length - 1 - n; i >= 0; i--) {
+                arr[i + n] = arr[i];
             }
-            for(int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 arr[i] = 0;
             }
         } else {
-            for(int i = 0 - n; i < arr.length; i++) {
-                arr[i+n] = arr[i];
+            for (int i = 0 - n; i < arr.length; i++) {
+                arr[i + n] = arr[i];
             }
-            for(int i = arr.length - 1; i > arr.length - 1 + n; i--) {
+            for (int i = arr.length - 1; i > arr.length - 1 + n; i--) {
                 arr[i] = 0;
             }
         }
     }
-
+    
     //Task 7 (Ring shift the entire array N times for one element)
     static void makeShiftRing(int[] arr, int n) {
-        if(n == 0) return;
-        if(Math.abs(n) >= arr.length) {
-                n = n % arr.length;
+        if (n == 0) return;
+        if (Math.abs(n) >= arr.length) {
+            n = n % arr.length;
         }
-
-        if(n > 0) {
-            for(int i = 0; i < n; i++) {
+        
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
                 int temp = arr[arr.length - 1];
                 makeShiftN(arr, 1);
                 arr[0] = temp;
             }
         } else {
-            for(int i = 0; i > n; i--) {
+            for (int i = 0; i > n; i--) {
                 int temp = arr[0];
                 makeShiftN(arr, -1);
                 arr[arr.length - 1] = temp;
             }
         }
     }
+    
     //Task 7 (Ring shift of the entire array 1 time by N elements)
     //Ring shift to the left by N elements is equivalent to shifting to the right by (arr.length - N) elements.
     static void makeShiftRingNoArray(int[] arr, int n) {
-        if(Math.abs(n) >= arr.length) {
+        if (Math.abs(n) >= arr.length) {
             n = n % arr.length;
         }
-
+        
         if (n == 0) return;
-
-        if(n < 0) {
+        
+        if (n < 0) {
             n = arr.length + n;
         }
-
+        
         int tmp1, tmp2;
         int cnt = 0;
         int currElem = 0;
-
+        
         tmp2 = arr[currElem];
-
-        while(cnt < arr.length) {
+        
+        while (cnt < arr.length) {
             int nextElem = (currElem + n) % arr.length;
-
+            
             // Handle the case of returning to a previously processed array element.
-            if((currElem + n) >= arr.length && ((cnt + 1) * n) % arr.length == 0) {
+            if ((currElem + n) >= arr.length && ((cnt + 1) * n) % arr.length == 0) {
                 tmp1 = arr[nextElem + 1];
                 currElem = nextElem + 1;
             } else {
                 tmp1 = arr[nextElem];
                 currElem = nextElem;
             }
-
+            
             arr[nextElem] = tmp2;
             tmp2 = tmp1;
-
+            
             cnt++;
         }
     }
