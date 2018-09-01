5bf27316ef33ed53ec15ca2ca35aab7174a431f9
DenisK <kuzovin@gmail.com>
2018-09-02 02:12:09 +0300

    hw7 + swing version

---
+++ HWLevel1Lesson7.java
@@ -0,0 +1,83 @@
+public class HWLevel1Lesson7 {
+    public static void main(String[] args) {
+        FeedTheCat feedTheCat = new FeedTheCat();
+
+        Plate plate = new Plate(50);
+
+        Cat[] cats = feedTheCat.callCats(5);
+        System.out.println(plate);
+        feedTheCat.feedCats(cats, plate);
+        System.out.println(plate);
+
+        plate.addFood(15);
+        System.out.println(plate);
+        feedTheCat.feedCats(cats, plate);
+        System.out.println(plate);
+
+    }
+}
+
+class FeedTheCat {
+    public Cat[] callCats(int count) {
+        Cat[] cats = new Cat[count];
+        for (int i = 0; i < cats.length; i++) {
+            cats[i] = new Cat((int) (Math.random() * 5 + 10));
+        }
+        return cats;
+    }
+
+    public void feedCats(Cat[] cats, Plate plate) {
+        for (int i = 0; i < cats.length; i++) {
+            if (cats[i].isHungry()) {
+                cats[i].eat(plate);
+            }
+        }
+    }
+}
+
+class Cat {
+    private int appetite;
+    private boolean hungry = true;
+
+    Cat(int appetite) {
+        this.appetite = appetite;
+    }
+
+    void eat(Plate plate) {
+        if (plate.decreaseFood(appetite)) hungry = false;
+    }
+
+    public boolean isHungry() {
+        return hungry;
+    }
+}
+
+class Plate {
+    private int food;
+
+    Plate(int food) {
+        this.food = food;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    boolean decreaseFood(int food) {
+        if (this.food >= food) {
+            this.food -= food;
+            return true;
+        }
+        return false;
+    }
+
+    public void addFood(int food) {
+        if (food > 0)
+            this.food += food;
+    }
+
+    @Override
+    public String toString() {
+        return "Food: " + food;
+    }
+}
